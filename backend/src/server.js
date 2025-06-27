fbdfb3fd8e5d70f09419a41deb1e19e697d51c5c
sandaliz <sandaliranahewa@gmail.com>
2025-06-27 10:10:25 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -12,11 +13,14 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5001; // Default port is 5001 if not specified in environment variables
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
 app.use(express.json()); //this will send req to ctrller
 app.use(rateLimiter);
 
@@ -26,6 +30,15 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))) //__dirname will go to backend so here what we do is
+    //go one up then tell it to go to frontend folder and use its dst as a static
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"))
+    });
+}
 
 connectDB().then(() => { //1st connect db then, listen - better option
     // Start the server on port PORT
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+//in production, theres no localhost, so url needs to be dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASE_URL,
     withCredentials: false,
 });
 
